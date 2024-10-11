4804ecc0d28360b32437dc5426c1d29516319e18
Abdullahi Muftau <abdullahimuftau2003@gmail.com>
2024-10-11 19:41:06 +0100

    some chnages

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -9,13 +9,13 @@ const Newsletter = () => {
     <section className="bg-purple-dozewell container mx-auto px-6 py-10 sm:px-20 sm:py-24 text-white space-y-4">
       <h1 className="text-xl md:text-5xl font-medium">Subscribe to Our Newsletter</h1>
 
-      <div className="flex justify-between bg-white items-center rounded-lg pl-2">
+      <div className="flex md:flex-row flex-col md:justify-between bg-white items-center rounded-lg pl-2">
         <input
           className="px-8 py-4 text-black outline-none"
           type="email"
           placeholder="Enter your email"
         />
-        <button className="bg-bg-primary px-2 md:px-8 py-4 rounded-r-lg">
+        <button className="bg-bg-primary px-6 sm:px-2 md:px-8 py-4 rounded-lg md:rounded-r-lg mb-4 md:mb-0">
           Subscribe
         </button>
       </div>
